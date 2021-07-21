27a05352b6255e097fb04bbf21c37ff3d2edd7bd
alazar-des <alazar.des@gmail.com>
2021-07-21 13:29:17 +0300

    existing left child parent set to new node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,6 +18,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		node->parent = parent;
 		node->left = parent->left;
 		node->right = NULL;
+		if (parent->left != NULL)
+			parent->left->parent = node;
 		parent->left = node;
 	}
 	return (node);
